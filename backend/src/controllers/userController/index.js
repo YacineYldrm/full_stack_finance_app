2d0fd0046364d6f0e2376dd05eb93015c2b8701b
YacineYldrm <yacine@mailbox.org>
2024-03-11 14:06:22 +0100

    Add user logout router and controller.

--- backend/src/controllers/userController/index.js
+++ backend/src/controllers/userController/index.js
@@ -2,11 +2,14 @@ import editCtrl from "./editCtrl.js";
 import loginCtrl from "./loginCtrl.js";
 import registerCtrl from "./registerCtrl.js";
 import getAllUserCtrl from "./getAllUserCtrl.js";
+import logoutCtrl from "./logoutCtrl.js";
+
 const userController = {
     getAllUser: getAllUserCtrl,
     edit: editCtrl,
     login: loginCtrl,
     register: registerCtrl,
+    logout: logoutCtrl,
 };
 
 export default userController;
