64d18c5576b87bef328b2a5cec80af12dfe03398
trisi99 <trisi.buschi@gmail.com>
2024-03-11 13:12:00 +0100

    Add login router, controller & services

--- backend/src/controllers/userController/index.js
+++ backend/src/controllers/userController/index.js
@@ -1,7 +1,9 @@
 import getAllUserCtrl from "./getAllUserCtrl.js";
+import { loginCtrl } from "./loginCtrl.js";
 
 const userController = {
     getAllUser: getAllUserCtrl,
+    login: loginCtrl
 };
 
 export default userController;
--- backend/src/services/userServices/index.js
+++ backend/src/services/userServices/index.js
@@ -1,7 +1,9 @@
 import getAllUser from "./getAllUser.js";
+import { loginService } from "./loginService.js";
 
 const userServices = {
     getAllUser,
+    loginService
 };
 
 export default userServices;
