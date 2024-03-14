718a9db5a6646b414a8a6016b3229b31f760ef53
YacineYldrm <yacine@mailbox.org>
2024-03-14 15:56:59 +0100

    edit all transactions component design

--- frontend/src/pages/AllTransaction/AllTransaction.jsx
+++ frontend/src/pages/AllTransaction/AllTransaction.jsx
@@ -1,149 +1,135 @@
-import logo from '../../../public/logo.png';
-import incomeIconBg from '../../../public/incomeIconBg.svg';
-import trendingUp from '../../../public/trendingUp.svg';
-import expenseIconBg from '../../../public/expenseIconBg.svg';
-import trendingDown from '../../../public/trendingDown.svg';
-import { useEffect, useState } from 'react';
-import TransactionDay from '../../components/TransactionDay/TransactionDay';
-import { backendUrl } from '../../api';
-import ModalAllTransaction from '../../components/ModalAllTransaction/ModalAllTransaction';
-import getAllAccounts from '../../utils/getAllAccounts';
-import Navbar from '../../components/Navbar/Navbar';
+import "./AllTransaction.scss";
+import logo from "../../../public/logo.png";
+import incomeIcon from "../../../public/incomeIcon.svg";
+import expenseIcon from "../../../public/expenseIcon.svg";
+import { useEffect, useState } from "react";
+import TransactionDay from "../../components/TransactionDay/TransactionDay";
+import { backendUrl } from "../../api";
+import ModalAllTransaction from "../../components/ModalAllTransaction/ModalAllTransaction";
+import getAllAccounts from "../../utils/getAllAccounts";
+import Navbar from "../../components/Navbar/Navbar";
 
 const AllTransaction = ({ provider }) => {
-	const [expenseTotal, setExpenseTotal] = useState(0);
-	const [incomeTotal, setIncomeTotal] = useState(0);
-	const [allDates, setAllDates] = useState([]);
-	const [dates, setDates] = useState([]);
-	const [modal, setModal] = useState(false);
+    const [expenseTotal, setExpenseTotal] = useState(0);
+    const [incomeTotal, setIncomeTotal] = useState(0);
+    const [allDates, setAllDates] = useState([]);
+    const [dates, setDates] = useState([]);
+    const [modal, setModal] = useState(false);
 
-	// #################################################
+    // #################################################
 
-	// #################################################
+    // #################################################
 
-	useEffect(() => {
-		if (provider.authorization) getAllAccounts(provider);
-	}, [provider.authorization]);
+    useEffect(() => {
+        if (provider.authorization) getAllAccounts(provider);
+    }, [provider.authorization]);
 
-	// #################################################
+    // #################################################
 
-	const getDates = () => {
-		let updatedDates = [];
-		provider.account?.transactions?.forEach((transaction) => {
-			const newdate = new Date(
-				transaction.date - new Date().getTimezoneOffset() * 60000,
-			)
-				.toISOString()
-				.slice(0, 10);
-			updatedDates = [...updatedDates, newdate];
-		});
-		setAllDates(updatedDates);
-	};
+    const getDates = () => {
+        let updatedDates = [];
+        provider.account?.transactions?.forEach((transaction) => {
+            const newdate = new Date(
+                transaction.date - new Date().getTimezoneOffset() * 60000
+            )
+                .toISOString()
+                .slice(0, 10);
+            updatedDates = [...updatedDates, newdate];
+        });
+        setAllDates(updatedDates);
+    };
 
-	// #################################################
+    // #################################################
 
-	useEffect(() => {
-		setDates([...new Set(allDates)]);
-	}, [allDates]);
+    useEffect(() => {
+        setDates([...new Set(allDates)]);
+    }, [allDates]);
 
-	// #################################################
+    // #################################################
 
-	useEffect(() => {
-		getDates();
-		const incomes = provider.account?.transactions?.filter(
-			(transaction) => transaction.type === 'income',
-		);
-		const expenses = provider.account?.transactions?.filter(
-			(transaction) => transaction.type === 'expense',
-		);
-		setIncomeTotal(incomes?.reduce((acc, curr) => acc + curr.amount, 0));
-		setExpenseTotal(expenses?.reduce((acc, curr) => acc + curr.amount, 0));
-	}, [provider.account]);
+    useEffect(() => {
+        getDates();
+        const incomes = provider.account?.transactions?.filter(
+            (transaction) => transaction.type === "income"
+        );
+        const expenses = provider.account?.transactions?.filter(
+            (transaction) => transaction.type === "expense"
+        );
+        setIncomeTotal(incomes?.reduce((acc, curr) => acc + curr.amount, 0));
+        setExpenseTotal(expenses?.reduce((acc, curr) => acc + curr.amount, 0));
+    }, [provider.account]);
 
-	// #################################################
+    // #################################################
 
-	return (
-		<>
-			{modal ? (
-				<ModalAllTransaction
-					transactions={provider.account.transactions}
-					modal={modal}
-					setModal={setModal}
-					provider={provider}
-				/>
-			) : null}
-			<header>
-				<img
-					src={logo}
-					alt='finco logo'
-				/>
-				<img
-					src={`http://localhost:3001/${provider.activeUser.profileImage}`}
-					alt='profile Image'
-				/>
-			</header>
-			<main>
-				<div>
-					<h1>All transaction</h1>
-					<div>
-						<img
-							src=''
-							alt='search'
-							onClick={() => setModal(true)}
-						/>
-						<img
-							src=''
-							alt='calendar'
-							onClick={()=>setModal(true)}
-						/>
-					</div>
-				</div>
-				<div>
-					<article>
-						<div>
-							<img
-								src={incomeIconBg}
-								alt='income icon'
-							/>
-							<img
-								src={trendingUp}
-								alt='income icon'
-							/>
-						</div>
-						<p>Income</p>
-						<h2>+${incomeTotal}</h2>
-					</article>
-					<article>
-						<div>
-							<img
-								src={expenseIconBg}
-								alt='expense icon'
-							/>
-							<img
-								src={trendingDown}
-								alt='expense icon'
-							/>
-						</div>
-						<p>Expense</p>
-						<h2>-${expenseTotal}</h2>
-					</article>
-				</div>
-				<section>
-					{dates
-						?.sort()
-						.reverse()
-						.map((date, i) => (
-							<TransactionDay
-								date={date}
-								key={i}
-								transactions={provider.account.transactions}
-							/>
-						))}
-				</section>
-			</main>
-			<Navbar />
-		</>
-	);
+    return (
+        <>
+            {modal ? (
+                <ModalAllTransaction
+                    transactions={provider.account.transactions}
+                    modal={modal}
+                    setModal={setModal}
+                    provider={provider}
+                />
+            ) : null}
+            <header>
+                <img src={logo} alt="finco logo" />
+                <img
+                    src={`http://localhost:3001/${provider.activeUser.profileImage}`}
+                    alt="profile Image"
+                />
+            </header>
+            <main className="all_transactions_main">
+                <div>
+                    <h1>All transactions</h1>
+                    <div>
+                        <img
+                            src=""
+                            alt="search"
+                            onClick={() => setModal(true)}
+                        />
+                        <img
+                            src=""
+                            alt="calendar"
+                            onClick={() => setModal(true)}
+                        />
+                    </div>
+                </div>
+                <div>
+                    <article>
+                        <div>
+                            <img src={incomeIcon} alt="income icon" />
+                        </div>
+                        <div>
+                            <p>Income</p>
+                            <h2>{incomeTotal?.toLocaleString()},00€</h2>
+                        </div>
+                    </article>
+                    <article>
+                        <div>
+                            <img src={expenseIcon} alt="expense icon" />
+                        </div>
+                        <div>
+                            <p>Expense</p>
+                            <h2>{expenseTotal?.toLocaleString()},00€</h2>
+                        </div>
+                    </article>
+                </div>
+                <section>
+                    {dates
+                        ?.sort()
+                        .reverse()
+                        .map((date, i) => (
+                            <TransactionDay
+                                date={date}
+                                key={i}
+                                transactions={provider.account.transactions}
+                            />
+                        ))}
+                </section>
+            </main>
+            <Navbar />
+        </>
+    );
 };
 
 export default AllTransaction;
--- frontend/src/pages/Register/Register.jsx
+++ frontend/src/pages/Register/Register.jsx
@@ -1,88 +1,85 @@
-import { Link, useNavigate } from 'react-router-dom';
-import logo from '../../../public/logo.png';
-import { useEffect, useState } from 'react';
-import './Register.scss';
-import Button from '../../components/Button/Button';
+import { Link, useNavigate } from "react-router-dom";
+import logo from "../../../public/logo.png";
+import { useEffect, useState } from "react";
+import "./Register.scss";
+import Button from "../../components/Button/Button";
 
 const Register = () => {
-	const [userInfo, setUserInfo] = useState({});
-	const [message, setMessage] = useState('');
-	const [accepted, setAccepted] = useState(false);
-	const navigate = useNavigate();
+    const [userInfo, setUserInfo] = useState({});
+    const [message, setMessage] = useState("");
+    const [accepted, setAccepted] = useState(false);
+    const navigate = useNavigate();
 
-	// #################################################
+    // #################################################
 
-	useEffect(() => {}, [message]);
+    useEffect(() => {}, [message]);
 
-	// #################################################
+    // #################################################
 
-	const handleRegistration = async () => {
-		event.preventDefault();
+    const handleRegistration = async () => {
+        event.preventDefault();
 
-		if (!accepted) {
-			setMessage('Please accept our Terms and Services!');
-			return;
-		}
+        if (!accepted) {
+            setMessage("Please accept our Terms and Services!");
+            return;
+        }
 
-		const response = await fetch(
-			'http://localhost:3001/api/v1/' + 'users/register',
-			{
-				method: 'POST',
-				headers: { 'Content-Type': 'application/json' },
-				body: JSON.stringify(userInfo),
-			},
-		);
+        const response = await fetch(
+            "http://localhost:3001/api/v1/" + "users/register",
+            {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(userInfo),
+            }
+        );
 
-		const { success, result, error, message } = await response.json();
-		if (!success) {
-			console.log(error, message);
-			setMessage(message);
-		} else {
-			setMessage('Great! Your registration was successful!');
-			navigate(`/verify/${result._id}`);
-		}
-	};
+        const { success, result, error, message } = await response.json();
+        if (!success) {
+            console.log(error, message);
+            setMessage(message);
+        } else {
+            setMessage("Thank you! Your registration was successful!");
+            navigate(`/verify/${result._id}`);
+        }
+    };
 
-	// #################################################
+    // #################################################
 
-	return (
-		<main className='registration'>
-			<div>
-				<img
-					src={logo}
-					alt='finco logo'
-				/>
-			</div>
-			<div>
-				<h1>Create an account</h1>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adip sicing elit,
-					sed do eiusmod.
-				</p>
-			</div>
-			<div>
-				<form>
-					<input
-						onChange={(e) =>
-							setUserInfo({
-								...userInfo,
-								firstName: e.target.value,
-							})
-						}
-						type='text'
-						placeholder='First Name'
-					/>
-					<input
-						onChange={(e) =>
-							setUserInfo({
-								...userInfo,
-								lastName: e.target.value,
-							})
-						}
-						type='text'
-						placeholder='Last Name'
-					/>
-					{/* <input
+    return (
+        <main className="registration">
+            <div>
+                <img src={logo} alt="finco logo" />
+            </div>
+            <div>
+                <h1>Create an account</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adip sicing elit,
+                    sed do eiusmod.
+                </p>
+            </div>
+            <div>
+                <form>
+                    <input
+                        onChange={(e) =>
+                            setUserInfo({
+                                ...userInfo,
+                                firstName: e.target.value,
+                            })
+                        }
+                        type="text"
+                        placeholder="First Name"
+                    />
+                    <input
+                        onChange={(e) =>
+                            setUserInfo({
+                                ...userInfo,
+                                lastName: e.target.value,
+                            })
+                        }
+                        type="text"
+                        placeholder="Last Name"
+                    />
+                    {/* <input
 						onChange={(e) =>
 							setUserInfo({
 								...userInfo,
@@ -92,61 +89,61 @@ const Register = () => {
 						type='date'
 						defaultValue={new Date().toISOString().slice(0, 10)}
 					/> */}
-					<input
-						onChange={(e) =>
-							setUserInfo({
-								...userInfo,
-								email: e.target.value,
-							})
-						}
-						type='text'
-						placeholder='Email'
-					/>
-					<input
-						onChange={(e) =>
-							setUserInfo({
-								...userInfo,
-								password: e.target.value,
-							})
-						}
-						type='password'
-						placeholder='Password'
-					/>
-					<input
-						onChange={(e) =>
-							setUserInfo({
-								...userInfo,
-								phoneNumber: e.target.value,
-							})
-						}
-						type='text'
-						placeholder='Phone'
-					/>
+                    <input
+                        onChange={(e) =>
+                            setUserInfo({
+                                ...userInfo,
+                                email: e.target.value,
+                            })
+                        }
+                        type="text"
+                        placeholder="Email"
+                    />
+                    <input
+                        onChange={(e) =>
+                            setUserInfo({
+                                ...userInfo,
+                                password: e.target.value,
+                            })
+                        }
+                        type="password"
+                        placeholder="Password"
+                    />
+                    <input
+                        onChange={(e) =>
+                            setUserInfo({
+                                ...userInfo,
+                                phoneNumber: e.target.value,
+                            })
+                        }
+                        type="text"
+                        placeholder="Phone"
+                    />
 
-					<label>
-						<input
-							onClick={() => setAccepted(true)}
-							type='checkbox'
-							name=''
-							id=''
-						/>{' '}
-						<p>
-							Agree to our <b>Terms and Services</b>
-						</p>
-					</label>
-				</form>
-			</div>
-			{message ? <h4>{message}</h4> : null}
+                    <label>
+                        <input
+                            onClick={() => setAccepted(true)}
+                            type="checkbox"
+                            name=""
+                            id=""
+                        />{" "}
+                        <p>
+                            Agree to our <b>Terms and Services</b>
+                        </p>
+                    </label>
+                </form>
+            </div>
+            {message ? <h4>{message}</h4> : null}
 
-			<Button
-				btnContent={'Register Now'}
-				btnFunction={handleRegistration}
-			/>
-			<p>
-				Already have an account? <Link to='/login'>Login</Link>
-			</p>
-		</main>
-	);
+            <Button
+                btnContent={"Register Now"}
+                btnFunction={handleRegistration}
+            />
+            <p>
+                Already have an account? <Link to="/login">Login</Link>
+            </p>
+        </main>
+    );
 };
 
 export default Register;
