bddd1d1ab13b86b9b4a189054f5fb9349d89885c
Samuel Aliyari <samuelaliyari@outlook.de>
2024-03-15 11:57:27 +0100

    add fixes renderissue & loop issue

---
+++ frontend/src/utils/calcTotal.js
@@ -0,0 +1,20 @@
+
+
+
+
+const calcTotal = (account, provider) => {
+
+    const incomes = account.transactions.filter(
+        (transaction) => transaction.type === 'income',
+    );
+    const expenses = account.transactions.filter(
+        (transaction) => transaction.type === 'expense',
+    );
+    provider.setIncomeTotal(incomes?.reduce((acc, curr) => acc + curr.amount, 0));
+    provider.setExpenseTotal(expenses?.reduce((acc, curr) => acc + curr.amount, 0));
+    console.log(provider)
+};
+
+
+
+export default calcTotal;
