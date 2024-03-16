ba47b11c192927b9e7572cf32873997e218b6939
Samuel Aliyari <samuelaliyari@outlook.de>
2024-03-16 21:45:37 +0100

    Add bugfixes report & daily Chart

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,119 +1,128 @@
-import { useEffect, useState } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.scss";
-import Home from "./pages/Home/Home";
-import Register from "./pages/Register/Register";
-import SetupAccount from "./pages/SetupAccount/SetupAccount";
-import Login from "./pages/Login/Login";
-import { silentRefresh } from "./utils/refresh";
-import AddIncome from "./pages/AddIncome/AddIncome";
-import AddExpense from "./pages/AddExpense/AddExpense";
-import AllTransaction from "./pages/AllTransaction/AllTransaction";
-import Reports from "./pages/Reports/Reports";
-import getAllAccounts from "./utils/getAllAccounts";
-import Verify from "./pages/Verify/Verify";
-import Menu from "./pages/Menu/Menu";
-import calcTotal from "./utils/calcTotal";
-import Settings from "./pages/Settings/Settings";
-import MyWallet from "./pages/MyWallet/MyWallet";
+import { useEffect, useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.scss';
+import Home from './pages/Home/Home';
+import Register from './pages/Register/Register';
+import SetupAccount from './pages/SetupAccount/SetupAccount';
+import Login from './pages/Login/Login';
+import { silentRefresh } from './utils/refresh';
+import AddIncome from './pages/AddIncome/AddIncome';
+import AddExpense from './pages/AddExpense/AddExpense';
+import AllTransaction from './pages/AllTransaction/AllTransaction';
+import Reports from './pages/Reports/Reports';
+import getAllAccounts from './utils/getAllAccounts';
+import Verify from './pages/Verify/Verify';
+import Menu from './pages/Menu/Menu';
+import calcTotal from './utils/calcTotal';
+import Settings from './pages/Settings/Settings';
+import MyWallet from './pages/MyWallet/MyWallet';
 
 function App() {
-    // #################################################
-    const [authorization, setAuthorization] = useState(null);
-    const [activeUser, setActiveUser] = useState("");
-    const [accounts, setAccounts] = useState([]);
-    const [account, setAccount] = useState(null);
-    const [transactions, setTransactions] = useState([]);
-    const [cardIndex, setCardIndex] = useState(0);
-    const [expenseTotal, setExpenseTotal] = useState(0);
-    const [incomeTotal, setIncomeTotal] = useState(0);
+	// #################################################
+	const [authorization, setAuthorization] = useState(null);
+	const [activeUser, setActiveUser] = useState('');
+	const [accounts, setAccounts] = useState([]);
+	const [account, setAccount] = useState(null);
+	const [transactions, setTransactions] = useState([]);
+	const [cardIndex, setCardIndex] = useState(0);
+	const [expenseTotal, setExpenseTotal] = useState(0);
+	const [incomeTotal, setIncomeTotal] = useState(0);
 
-    const provider = {
-        authorization,
-        setAuthorization,
-        activeUser,
-        setActiveUser,
-        accounts,
-        setAccounts,
-        account,
-        setAccount,
-        transactions,
-        setTransactions,
-        cardIndex,
-        setCardIndex,
-        expenseTotal,
-        setExpenseTotal,
-        incomeTotal,
-        setIncomeTotal,
-    };
+	const provider = {
+		authorization,
+		setAuthorization,
+		activeUser,
+		setActiveUser,
+		accounts,
+		setAccounts,
+		account,
+		setAccount,
+		transactions,
+		setTransactions,
+		cardIndex,
+		setCardIndex,
+		expenseTotal,
+		setExpenseTotal,
+		incomeTotal,
+		setIncomeTotal,
+	};
 
-    // ############### Refresh Token on pagereload ###############
+	// ############### Refresh Token on pagereload ###############
 
-    useEffect(() => {
-        if (!authorization)
-            silentRefresh(null, setAuthorization, setActiveUser);
-    }, []);
+	useEffect(() => {
+		if (!authorization)
+			silentRefresh(null, setAuthorization, setActiveUser);
+	}, []);
 
-    useEffect(() => {
-        if (provider.authorization) getAllAccounts(provider);
-    }, [authorization]);
-    useEffect(() => {
-        if (account) calcTotal(account, provider);
-    }, [incomeTotal, expenseTotal]);
+	useEffect(() => {
+		if (provider.authorization) getAllAccounts(provider);
+	}, [authorization]);
 
-    // #################################################
+	// #################################################
 
-    return (
-        <>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Home provider={provider} />} />
+	useEffect(() => {
+		if (account) calcTotal(account, provider);
+	}, [incomeTotal, expenseTotal]);
 
-                    <Route
-                        path="/register"
-                        element={<Register provider={provider} />}
-                    />
-                    <Route
-                        path="/account/setup"
-                        element={<SetupAccount provider={provider} />}
-                    />
-                    <Route
-                        path="/login"
-                        element={<Login provider={provider} />}
-                    />
-                    <Route
-                        path="/add-income"
-                        element={<AddIncome provider={provider} />}
-                    />
-                    <Route
-                        path="add-expense"
-                        element={<AddExpense provider={provider} />}
-                    />
-                    <Route
-                        path="/all-transactions"
-                        element={<AllTransaction provider={provider} />}
-                    />
-                    <Route
-                        path="/reports"
-                        element={<Reports provider={provider} />}
-                    />
-                    <Route path="/verify/:userId" element={<Verify />} />
-                    <Route
-                        path="/menu"
-                        element={<Menu provider={provider} />}
-                    />
-                    <Route
-                        path="/menu/settings"
-                        element={<Settings provider={provider} />}
-                    />
-                    <Route
-                        path="/menu/my-wallet"
-                        element={<MyWallet provider={provider} />}
-                    />
-                </Routes>
-            </BrowserRouter>
-        </>
-    );
+	// #################################################
+
+	return (
+		<>
+			<BrowserRouter>
+				<Routes>
+					<Route
+						path='/'
+						element={<Home provider={provider} />}
+					/>
+
+					<Route
+						path='/register'
+						element={<Register provider={provider} />}
+					/>
+					<Route
+						path='/account/setup'
+						element={<SetupAccount provider={provider} />}
+					/>
+					<Route
+						path='/login'
+						element={<Login provider={provider} />}
+					/>
+					<Route
+						path='/add-income'
+						element={<AddIncome provider={provider} />}
+					/>
+					<Route
+						path='add-expense'
+						element={<AddExpense provider={provider} />}
+					/>
+					<Route
+						path='/all-transactions'
+						element={<AllTransaction provider={provider} />}
+					/>
+					<Route
+						path='/reports'
+						element={<Reports provider={provider} />}
+					/>
+					<Route
+						path='/verify/:userId'
+						element={<Verify />}
+					/>
+					<Route
+						path='/menu'
+						element={<Menu provider={provider} />}
+					/>
+					<Route
+						path='/menu/settings'
+						element={<Settings provider={provider} />}
+					/>
+					<Route
+						path='/menu/my-wallet'
+						element={<MyWallet provider={provider} />}
+					/>
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
 }
 
 export default App;
--- frontend/src/pages/Reports/Reports.jsx
+++ frontend/src/pages/Reports/Reports.jsx
@@ -1,285 +1,288 @@
-import "./Reports.scss";
-import logo from "../../../public/Logo.svg";
-import incomeIcon from "../../../public/incomeIcon.svg";
-import expenseIcon from "../../../public/expenseIcon.svg";
-import { useEffect, useState } from "react";
-import Transaction from "../../components/Transaction/Transaction";
-import Navbar from "../../components/Navbar/Navbar";
-import ReactECharts from "echarts-for-react";
-import Arrow from "../../../public/svg/Arrows/Arrow";
-import { useNavigate } from "react-router-dom";
-import getAllAccounts from "../../utils/getAllAccounts";
-import { mediaUrl } from "../../api";
+import './Reports.scss';
+import logo from '../../../public/Logo.svg';
+import incomeIcon from '../../../public/incomeIcon.svg';
+import expenseIcon from '../../../public/expenseIcon.svg';
+import { useEffect, useState } from 'react';
+import Transaction from '../../components/Transaction/Transaction';
+import Navbar from '../../components/Navbar/Navbar';
+import ReactECharts from 'echarts-for-react';
+import Arrow from '../../../public/svg/Arrows/Arrow';
+import { useNavigate } from 'react-router-dom';
+import getAllAccounts from '../../utils/getAllAccounts';
+import { mediaUrl } from '../../api';
 // import BarChart from "../../components/Charts/BarChart";
 
 const Reports = ({ provider }) => {
-    const [expenseTotal, setExpenseTotal] = useState(0);
-    const [incomeTotal, setIncomeTotal] = useState(0);
-    const [allTransactions, setAllTransactions] = useState([]);
-    const [renderedTransactions, setRenderedTransactions] = useState([]);
-    const [index, setIndex] = useState(5);
+	const [expenseTotal, setExpenseTotal] = useState(0);
+	const [incomeTotal, setIncomeTotal] = useState(0);
+	const [allTransactions, setAllTransactions] = useState([]);
+	const [renderedTransactions, setRenderedTransactions] = useState([]);
+	const [index, setIndex] = useState(5);
 
-    const navigate = useNavigate();
+	const navigate = useNavigate();
 
-    // ####################################################################
-    useEffect(() => {
-        getAllAccounts(provider);
-        provider.setAccount(provider.accounts[provider.cardIndex]);
-    }, [provider, provider.authorization]);
+	// ####################################################################
+	// useEffect(() => {
+	//     getAllAccounts(provider);
+	//     provider.setAccount(provider.accounts[provider.cardIndex]);
+	// }, [provider, provider.authorization]);
 
-    // #######################render loadmore #############################
+	// #######################render loadmore #############################
 
-    useEffect(() => {
-        setRenderedTransactions(allTransactions?.slice(0, index));
-    }, [index, allTransactions]);
+	useEffect(() => {
+		setRenderedTransactions(allTransactions?.slice(0, index));
+	}, [index, allTransactions]);
 
-    // #########################Total calc################################
+	// #########################Total calc################################
 
-    useEffect(() => {
-        const incomes = renderedTransactions?.filter(
-            (transaction) => transaction.type === "income"
-        );
-        const expenses = renderedTransactions?.filter(
-            (transaction) => transaction.type === "expense"
-        );
-        setIncomeTotal(incomes?.reduce((acc, curr) => acc + curr.amount, 0));
-        setExpenseTotal(expenses?.reduce((acc, curr) => acc + curr.amount, 0));
-    }, [renderedTransactions]);
+	useEffect(() => {
+		const incomes = renderedTransactions?.filter(
+			(transaction) => transaction.type === 'income',
+		);
+		const expenses = renderedTransactions?.filter(
+			(transaction) => transaction.type === 'expense',
+		);
+		setIncomeTotal(incomes?.reduce((acc, curr) => acc + curr.amount, 0));
+		setExpenseTotal(expenses?.reduce((acc, curr) => acc + curr.amount, 0));
+	}, [renderedTransactions]);
 
-    // ###############################################################
+	// ###############################################################
 
-    useEffect(() => {
-        setAllTransactions(
-            provider?.account?.transactions?.sort(
-                (transOne, transTwo) => transTwo.date - transOne.date
-            )
-        );
-    }, [provider.account]);
+	useEffect(() => {
+		setAllTransactions(
+			provider?.account?.transactions?.sort(
+				(transOne, transTwo) => transTwo.date - transOne.date,
+			),
+		);
+	}, [provider.account]);
 
-    // ###############################################################
+	// ###############################################################
+	const dailyView = {
+		legend: [{ show: true, itemGap: 30 }],
+		tooltip: {},
+		animationEasing: 'circularIn',
+		color: ['#da6e53'],
+		textStyle: {
+			fontFamily: 'urbanist',
+			fontSize: '16px',
+		},
+		grid: {
+			left: 10,
+			containLabel: true,
+			bottom: 10,
+			top: 50,
+			right: 10,
+		},
+		dataset: {
+			dimensions: ['period', 'expense'],
+			source: [],
+		},
+		xAxis: { type: 'category' },
+		yAxis: { axisLabel: false, splitNumber: 6 },
+		// Declare several bar series, each will be mapped
+		// to a column of dataset.source by default.
+		series: [
+			{
+				type: 'line',
+				smooth: true,
+				areaStyle: {
+					color: '#da6e53',
+					origin: 'start',
+				},
+			},
+			{
+				type: 'none',
+			},
+		],
+	};
 
-    const monthlyView = {
-        legend: [{ show: true, itemGap: 30 }],
-        tooltip: {},
-        animationEasing: "circularIn",
-        color: ["#409474", "#da6e53"],
-        textStyle: {
-            fontFamily: "urbanist",
-            fontSize: "16px",
-        },
-        grid: {
-            left: 10,
-            containLabel: true,
-            bottom: 10,
-            top: 50,
-            right: 10,
-        },
-        dataset: {
-            dimensions: ["period", "income", "expense"],
-            source: [
-                { period: "Jan", income: 0, expense: 0 },
-                { period: "Feb", income: 0, expense: 0 },
-                { period: "Mar", income: 0, expense: 0 },
-                { period: "Apr", income: 0, expense: 0 },
-                { period: "May", income: 0, expense: 0 },
-                { period: "Jun", income: 0, expense: 0 },
-                { period: "Jul", income: 0, expense: 0 },
-                { period: "Aug", income: 0, expense: 0 },
-                { period: "Sep", income: 0, expense: 0 },
-                { period: "Oct", income: 0, expense: 0 },
-                { period: "Nov", income: 0, expense: 0 },
-                { period: "Dec", income: 0, expense: 0 },
-            ],
-        },
-        xAxis: { type: "category" },
-        yAxis: { axisLabel: false, splitNumber: 6 },
-        // Declare several bar series, each will be mapped
-        // to a column of dataset.source by default.
-        series: [{ type: "bar" }, { type: "bar" }],
-    };
+	// ###############################################################
 
-    const dailyView = {
-        legend: [{ show: true, itemGap: 30 }],
-        tooltip: {},
-        animationEasing: "circularIn",
-        color: ["#da6e53", "#409474"],
-        textStyle: {
-            fontFamily: "urbanist",
-            fontSize: "16px",
-        },
-        grid: {
-            left: 10,
-            containLabel: true,
-            bottom: 10,
-            top: 50,
-            right: 10,
-        },
-        dataset: {
-            dimensions: ["period", "expense", "difference"],
-            source: [],
-        },
-        xAxis: { type: "category" },
-        yAxis: { axisLabel: false, splitNumber: 6 },
-        // Declare several bar series, each will be mapped
-        // to a column of dataset.source by default.
-        series: [
-            {
-                type: "line",
-                smooth: true,
-                areaStyle: {
-                    color: "#da6e53",
-                    origin: "start",
-                },
-            },
-            {
-                type: "line",
-            },
-        ],
-    };
+	const setMonthlyView = () => {
+		const source = [...monthlyView.dataset.source];
+		const transactions = provider?.account?.transactions;
 
-    // ###############################################################
+		transactions?.forEach((transaction) => {
+			const transactionDate = new Date(transaction.date).getMonth();
+			source[transactionDate][transaction.type] =
+				source[transactionDate][transaction.type] + transaction.amount;
+		});
+	};
 
-    const setMonthlyView = () => {
-        const source = [...monthlyView.dataset.source];
-        const transactions = provider?.account?.transactions;
+	const setDailyView = () => {
+		const transactions = provider?.account?.transactions;
 
-        transactions?.forEach((transaction) => {
-            const transactionDate = new Date(transaction.date).getMonth();
-            source[transactionDate][transaction.type] =
-                source[transactionDate][transaction.type] + transaction.amount;
-        });
-    };
+		transactions?.forEach((transaction) => {
+			const transactionYear = new Date(transaction.date).getFullYear();
+			const transactionMonth = new Date(Date.now()).getMonth();
+			const calcDay = new Date(Date.now()).getDate();
+			// const monthLength = new Date(
+			// 	transactionYear,
+			// 	transactionMonth + 1,
+			// 	0,
+			// ).getDate();
+			let sourceArray = [];
 
-    const setDailyView = () => {
-        const transactions = provider?.account?.transactions;
+			for (let i = 1; i <= calcDay; i++) {
+				sourceArray = [
+					...sourceArray,
+					{
+						period: i,
+						expense: 0,
+					},
+				];
+			}
+			const expenseTransactions = transactions?.filter(
+				(transaction) => transaction.type === 'expense',
+			);
 
-        transactions?.forEach((transaction) => {
-            const transactionYear = new Date(transaction.date).getFullYear();
-            const transactionMonth = new Date(Date.now()).getMonth();
-            const calcDay = new Date(Date.now()).getDate();
-            // const monthLength = new Date(
-            // 	transactionYear,
-            // 	transactionMonth + 1,
-            // 	0,
-            // ).getDate();
-            let sourceArray = [];
+			expenseTransactions?.forEach((transaction) => {
+				const transactionDay = new Date(transaction.date).getDate();
+				if (transactionDay <= calcDay) {
+					sourceArray[transactionDay - 1][transaction.type] =
+						sourceArray[transactionDay - 1][transaction.type] +
+						transaction.amount;
+				}
+			});
+			dailyView.dataset.source = sourceArray;
+		});
+	};
 
-            for (let i = 1; i <= calcDay; i++) {
-                sourceArray = [
-                    ...sourceArray,
-                    {
-                        period: i,
-                        expense: 0,
-                    },
-                ];
-            }
-            const expenseTransactions = transactions?.filter(
-                (transaction) => transaction.type === "expense"
-            );
+	const monthlyView = {
+		legend: [{ show: true, itemGap: 30 }],
+		tooltip: {},
+		animationEasing: 'circularIn',
+		color: ['#da6e53', '#409474'],
+		textStyle: {
+			fontFamily: 'urbanist',
+			fontSize: '16px',
+		},
+		grid: {
+			left: 10,
+			containLabel: true,
+			bottom: 10,
+			top: 50,
+			right: 10,
+		},
+		dataset: {
+			dimensions: ['period', 'expense', 'income'],
+			source: [
+				{ period: 'Jan', income: 0, expense: 0 },
+				{ period: 'Feb', income: 0, expense: 0 },
+				{ period: 'Mar', income: 0, expense: 0 },
+				{ period: 'Apr', income: 0, expense: 0 },
+				{ period: 'May', income: 0, expense: 0 },
+				{ period: 'Jun', income: 0, expense: 0 },
+				{ period: 'Jul', income: 0, expense: 0 },
+				{ period: 'Aug', income: 0, expense: 0 },
+				{ period: 'Sep', income: 0, expense: 0 },
+				{ period: 'Oct', income: 0, expense: 0 },
+				{ period: 'Nov', income: 0, expense: 0 },
+				{ period: 'Dec', income: 0, expense: 0 },
+			],
+		},
+		xAxis: { type: 'category' },
+		yAxis: { axisLabel: false, splitNumber: 6 },
+		// Declare several bar series, each will be mapped
+		// to a column of dataset.source by default.
+		series: [{ type: 'bar' }, { type: 'bar' }],
+	};
 
-            expenseTransactions?.forEach((transaction) => {
-                const transactionDay = new Date(transaction.date).getDate();
-                if (transactionDay <= calcDay) {
-                    sourceArray[transactionDay - 1][transaction.type] =
-                        sourceArray[transactionDay - 1][transaction.type] +
-                        transaction.amount;
-                }
-            });
-            dailyView.dataset.source = sourceArray;
-        });
-    };
+	setMonthlyView();
+	setDailyView();
 
-    setMonthlyView();
-    setDailyView();
+	const [dailyOption, setDailyOption] = useState(monthlyView);
+	// ###############################################################
 
-    const [dailyOption, setDailyOption] = useState(monthlyView);
-    // ###############################################################
-
-    return (
-        <>
-            <main className="report_main">
-                <section>
-                    <div className="userInfo">
-                        <Arrow onClick={() => navigate("/")} />
-                        <img
-                            src={`${mediaUrl}${provider.activeUser?.profileImage}`}
-                            alt=""
-                        />
-                    </div>
-                    <h1>Report</h1>
-                    <div>
-                        <article>
-                            <div>
-                                <img src={incomeIcon} alt="income icon" />
-                            </div>
-                            <div>
-                                <p>Income</p>
-                                <h2>
-                                    {incomeTotal?.toLocaleString("de-DE", {
-                                        style: "currency",
-                                        currency: "EUR",
-                                    })}
-                                </h2>
-                            </div>
-                        </article>
-                        <article>
-                            <div>
-                                <img src={expenseIcon} alt="expense icon" />
-                            </div>
-                            <div>
-                                <p>Expense</p>
-                                <h2>
-                                    {expenseTotal?.toLocaleString("de-DE", {
-                                        style: "currency",
-                                        currency: "EUR",
-                                    })}
-                                </h2>
-                            </div>
-                        </article>
-                    </div>
-                    <article className="bar_chart">
-                        <ReactECharts option={dailyOption} />
-                        <article onClick={() => setDailyOption(monthlyView)}>
-                            Monthly
-                        </article>
-                        <article onClick={() => setDailyOption(dailyView)}>
-                            Daily
-                        </article>
-                    </article>
-                </section>
-                <section>
-                    {renderedTransactions?.map((transaction) => (
-                        <Transaction
-                            key={transaction._id}
-                            transaction={transaction}
-                        />
-                    ))}
-                </section>
-                <section>
-                    {index < allTransactions?.length ? (
-                        <p
-                            onClick={() => {
-                                index < allTransactions?.length
-                                    ? setIndex(index + 5)
-                                    : null;
-                            }}
-                        >
-                            load more
-                        </p>
-                    ) : null}
-                    {index <= 5 || allTransactions?.length <= 5 ? null : (
-                        <p
-                            onClick={() => {
-                                index !== 5 ? setIndex(index - 5) : null;
-                            }}
-                        >
-                            show less
-                        </p>
-                    )}
-                </section>
-            </main>
-        </>
-    );
+	return (
+		<>
+			<main className='report_main'>
+				<section>
+					<div className='userInfo'>
+						<Arrow onClick={() => navigate('/')} />
+						<img
+							src={`${mediaUrl}${provider.activeUser?.profileImage}`}
+							alt=''
+						/>
+					</div>
+					<h1>Report</h1>
+					<div>
+						<article>
+							<div>
+								<img
+									src={incomeIcon}
+									alt='income icon'
+								/>
+							</div>
+							<div>
+								<p>Income</p>
+								<h2>
+									{incomeTotal?.toLocaleString('de-DE', {
+										style: 'currency',
+										currency: 'EUR',
+									})}
+								</h2>
+							</div>
+						</article>
+						<article>
+							<div>
+								<img
+									src={expenseIcon}
+									alt='expense icon'
+								/>
+							</div>
+							<div>
+								<p>Expense</p>
+								<h2>
+									{expenseTotal?.toLocaleString('de-DE', {
+										style: 'currency',
+										currency: 'EUR',
+									})}
+								</h2>
+							</div>
+						</article>
+					</div>
+					<article className='bar_chart'>
+						<ReactECharts option={dailyOption} />
+						<article onClick={() => setDailyOption(monthlyView)}>
+							Monthly
+						</article>
+						<article onClick={() => setDailyOption(dailyView)}>
+							Daily
+						</article>
+					</article>
+				</section>
+				<section>
+					{renderedTransactions?.map((transaction) => (
+						<Transaction
+							key={transaction._id}
+							transaction={transaction}
+						/>
+					))}
+				</section>
+				<section>
+					{index < allTransactions?.length ? (
+						<p
+							onClick={() => {
+								index < allTransactions?.length
+									? setIndex(index + 5)
+									: null;
+							}}>
+							load more
+						</p>
+					) : null}
+					{index <= 5 || allTransactions?.length <= 5 ? null : (
+						<p
+							onClick={() => {
+								index !== 5 ? setIndex(index - 5) : null;
+							}}>
+							show less
+						</p>
+					)}
+				</section>
+			</main>
+		</>
+	);
 };
 
 export default Reports;
