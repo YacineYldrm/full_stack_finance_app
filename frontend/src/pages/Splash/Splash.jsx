a5cca769bb852dd974d8159c917bb6e3b7655b65
Samuel Aliyari <samuelaliyari@outlook.de>
2024-03-17 22:32:51 +0100

    Add  animation & design to Splash, onBoarding & getStarted Pages

--- frontend/src/pages/Splash/Splash.jsx
+++ frontend/src/pages/Splash/Splash.jsx
@@ -1,26 +1,46 @@
 import { useEffect } from 'react';
-import logo from '../../../public/logo.png'
-import './Splash.scss'
+import left from '../../../public/splash/left.svg';
+import right from '../../../public/splash/right.svg';
+import circle from '../../../public/splash/circle.svg';
+import './Splash.scss';
 import { useNavigate } from 'react-router-dom';
 
 const Splash = () => {
-const navigate = useNavigate()
-    useEffect(()=>{
-        setTimeout(()=>{
-            navigate("/onboard")
-        },5000)
-    },[])
-    return ( 
-        <>
-        <main>
-            <section>
-                <div>
-                    <img src={logo} alt="Finco logo" />
-                </div>
-            </section>
-        </main>
-        </>
-     );
-}
- 
+	const navigate = useNavigate();
+	useEffect(() => {
+		setTimeout(() => {
+			navigate('/onboard');
+		}, 4000);
+	}, []);
+	return (
+		<>
+			<main className='splash'>
+				<section>
+					<div>
+						<img
+							className='left'
+							src={left}
+							alt='Finco logo'
+						/>
+					</div>
+					<div>
+						<img
+							className='right'
+							src={right}
+							alt='Finco logo'
+						/>
+					</div>
+					<div>
+						<img
+							className='circle'
+							src={circle}
+							alt='Finco logo'
+						/>
+					</div>
+				</section>
+			</main>
+		</>
+	);
+};
+
 export default Splash;
