8fa5cef2ef0ddfbea6c3119601d9e595bfe97995
YacineYldrm <yacine@mailbox.org>
2024-03-18 20:38:56 +0100

    unfinished: show active card function

--- frontend/src/components/CardCarouselle/CardCarouselle.jsx
+++ frontend/src/components/CardCarouselle/CardCarouselle.jsx
@@ -1,15 +1,50 @@
-import { useEffect } from 'react';
-import changeCardOnSwipe from '../../utils/changeCardOnSwipe';
-import Card from '../Card/Card';
-import showActiveCard from '../../utils/showActiveCard';
-
+import { useEffect, useMemo, useState } from "react";
+import _ from "lodash";
+import changeCardOnSwipe from "../../utils/changeCardOnSwipe";
+import Card from "../Card/Card";
+import showActiveCard from "../../utils/showActiveCard";
+import activeCardIndex from "../../utils/activeCardIndex";
 
 const CardCourouselle = ({ provider }) => {
+    const [index, setIndex] = useState(null);
+    // const [foundCard, setFoundCard] = useState(null);
+
+    // useEffect(() => {
+    //     const index = activeCardIndex(provider);
+    //     setFoundCard(index);
+    // }, [provider]);
+
+    // useEffect(() => {
+    //     console.log("foundCard", foundCard);
+    //     console.log("activeCardIndex", activeCardIndex(provider));
+    //     provider?.setCardIndex(activeCardIndex(provider));
+    //     setIndex(activeCardIndex(provider));
+    // }, [foundCard]);
+
+    // useEffect(() => {
+    //     console.log("indexState", index);
+    //     showActiveCard(provider);
+    // }, [index]);
+
+    const handleScroll = (e) => {
+        showActiveCard(provider);
+    };
+
+    // const handleEndScroll = useMemo(
+    //     () =>
+    //         _.debounce(() => {
+
+    //         }, 3000),
+    //     []
+    // );
+
     return (
         <article
-            onScroll={() => provider?.setCardIndex(changeCardOnSwipe())}
-
-            className='cards_carouselle_wrapper'>
+            onScroll={(e) => handleScroll(e)}
+            // onScroll={() => provider?.setCardIndex(changeCardOnSwipe())}
+            className="cards_carouselle_wrapper"
+            id="carouselle"
+        >
             {provider.accounts.map((account) => {
                 return (
                     <Card
--- frontend/src/utils/showActiveCard.js
+++ frontend/src/utils/showActiveCard.js
@@ -1,19 +1,29 @@
-
+import changeCardOnSwipe from "./changeCardOnSwipe";
 
 const getCards = () => {
     const cards = document.body.getElementsByClassName("card");
-
-    return cards
+    return cards;
 };
 
 const showActiveCard = (provider) => {
-    const cards = getCards()
-    console.log(cards[provider?.cardIndex])
-    const activeCard = cards[provider?.cardIndex]
-    // scrollIntoView({ left: activeCard.offsetLeft })
-    console.log(activeCard)
-    scrollTo(activeCard)
-}
+    const carouselle = document.getElementById("carouselle");
+    // console.log(carouselle);
+    const cards = getCards();
+    // console.log(cards);
+    const activeCard = cards[provider?.cardIndex];
+    // console.log(activeCard.getBoundingClientRect());
+    const activeCardPosition = activeCard?.getBoundingClientRect();
 
+    if (activeCardPosition?.left > 0 && activeCardPosition?.right > 0) {
+        console.log("changing card index");
+        provider?.setCardIndex(changeCardOnSwipe());
+        return;
+    } else {
+        setTimeout(() => {
+            console.log("active card not in view");
+            carouselle?.scrollTo(activeCardPosition?.x, activeCardPosition?.y);
+        }, 1000);
+    }
+};
 
 export default showActiveCard;
